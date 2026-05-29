fb1f1e2717c215bab9cc3a9911f3f9ab0f2ef898
karen-elkess <karenelbaba2024@gmail.com>
2026-05-29 17:32:20 +0300

    Initial commit - Grocerry App started

---
+++ lib/grocery_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/login_screen.dart';
+
+class GroceryApp extends StatelessWidget {
+  const GroceryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      home: LoginScreen(
+        
+      ),
+    );
+  }
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/home_screen.dart';
+import 'package:grocery_app/register_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment:CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20,),
+
+                Center(
+                  child: Image.asset("assets/images/Group.png" ,
+                  width: 120,
+                  height: 120,
+                  ),
+                ),
+                SizedBox(height: 55,) ,
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text("Login" ,
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w700 ,
+                  ),
+                  ),
+                ),
+                
+                Text("Enter your emails and password" ,
+                style: TextStyle(
+                  fontSize:20 ,
+                  fontWeight:FontWeight.w400 ,
+                  color: Color.fromARGB(255, 157, 154, 154),
+                ),
+                ),
+                SizedBox(height:40,),
+                Text("Email",
+                style: TextStyle(
+                  fontSize:25 ,
+                  fontWeight:FontWeight.w500 ,
+                  color: Colors.grey,
+                ),
+                ),
+                TextFormField(
+                decoration: InputDecoration(
+                  suffix: Icon(Icons.email),
+                ),
+                ),
+                SizedBox(height:40,),
+                Text("Password",
+                style: TextStyle(
+                  fontSize:25 ,
+                  fontWeight:FontWeight.w500 ,
+                  color: Colors.grey,
+                ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                decoration: InputDecoration(
+                  suffix: Icon(Icons.visibility),
+                ),
+                ),
+                SizedBox(height: 25,),
+
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Text("Forgot Password?" ,
+                  style: TextStyle(
+                    fontSize: 20 ,
+                    fontWeight: FontWeight.w600
+                  ),
+                  ),
+                ),
+                SizedBox(height: 15,),
+                
+                InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                     MaterialPageRoute(builder: (context)=> RegisterScreen()));
+                  },
+                  child: Container(
+                    width: 370,
+                    height: 85,
+                    
+                    decoration: BoxDecoration(
+                     color:  Color(0xff53B175),
+                     borderRadius: BorderRadius.circular(14),
+                     ),
+                    child: Align(
+                      child: Text("Log in" ,
+                                      style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500 ,
+                      color: Colors.white,
+                                      ),
+                                      ),
+                    ) ,
+                  ),
+                ),
+                
+                SizedBox(height: 22,),
+                Row( 
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                Text("Don’t have an account?" ,
+                style: TextStyle(
+                  fontSize:18 ,
+                  fontWeight:FontWeight.w500 ,
+                ),
+                ),
+                Text(" Signup",
+                style: TextStyle(
+                  fontSize:18 ,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xff53B175),
+                ),
+                )
+                 ]
+                )
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+}
